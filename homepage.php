eb6542e25a8b576eec992f07b289ce01fbffb7cf
Renzzzz-Git <114047086+Renzzzz-Git@users.noreply.github.com>
2023-05-05 22:41:41 +0800

    re-arrange homepage files part-2

--- homepage.php
+++ homepage.php
@@ -11,7 +11,7 @@
 <html>
 <head>
 
-  <link rel="stylesheet" href="homepage.css">
+  <link rel="stylesheet" href="css/homepage.css">
 
 
 
@@ -33,12 +33,12 @@
 
 
 <div class="bg">
-<img src="luxlobby2.jpg" width="100%" height="670">
-  <div class="greet"><img src="Welcome to Firefly Hotel (2).png" width="800" height="500"></div>
+<img src="photos/luxlobby2.jpg" width="100%" height="670">
+  <div class="greet"><img src="photos/Welcome to Firefly Hotel (2).png" width="800" height="500"></div>
   <div class="rooms"><a class="links" href="#suites">Suites</a></li></div>
   <div class="book"><a class="links" href="#book">Book</a></div>
 
-  <div class="rate"><img src="5 star rev2.png" width="400" height="250"></div>
+  <div class="rate"><img src="photos/5 star rev2.png" width="400" height="250"></div>
 
 
 </div>
