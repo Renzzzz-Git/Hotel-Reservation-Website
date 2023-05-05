1258c1965bee9d92df99c909a82ed870448cef4a
Renzzzz-Git <gdevrenzz055@gmail.com>
2023-05-06 00:51:52 +0800

    update css path for index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, intitial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Hotel Reservation System</title>
-	<link href="../css/login.css"  rel = "stylesheet" type = "text/css" >
+	<link href="css/login.css"  rel = "stylesheet" type = "text/css" >
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
 <body>
