2405b6bdb4e9cb28422940ee9e2fc75e7273d4fa
noelev99-at-Github <noelenreravalencia99@gmail.com>
2023-05-09 15:27:55 +0800

    first commit ni noel

---
+++ Php/login.php
@@ -0,0 +1,39 @@
+<?php
+	//LOGIN CODE FOR CONNECTING MEMBER TO DATABASE
+	if(ISSET ($_POST['member_login'])){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+        session_start();
+        $conn = mysqli_connect('localhost', 'root', '', 'hotel-reservation-system') or die('Unable to connect');
+		$query = $conn->query("SELECT * FROM `members` WHERE `username` = '$username' && `password` = '$password'") or die(mysqli_error());
+		$fetch = $query->fetch_array();
+		$row = $query->num_rows;
+		
+		if($row > 0){
+			$_SESSION['username'] = $fetch['username'];
+			header('location:chat.php');
+		}else{
+			echo "<center><label style = 'color:red;'>Invalid username or password</label></center>";
+		}
+	}
+
+
+
+	//LOGIN CODE FOR CONNECTING STAFF TO DATABASE
+	if(ISSET ($_POST['staff_login'])){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+        session_start();
+        $conn = mysqli_connect('localhost', 'root', '', 'hotel-reservation-system') or die('Unable to connect');
+		$query = $conn->query("SELECT * FROM `employees` WHERE `username` = '$username' && `password` = '$password'") or die(mysqli_error());
+		$fetch = $query->fetch_array();
+		$row = $query->num_rows;
+		
+		if($row > 0){
+			$_SESSION['username'] = $fetch['username'];
+			header('location:timeClockPage.php');
+		}else{
+			echo "<center><label style = 'color:red;'>Invalid username or password</label></center>";
+		}
+	}
+?>
---
+++ member/chat.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['username'])){
+	header('location:../homepage.php');
+	
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, intitial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Hotel Reservation System</title>
+	<link href="../css/chat.css"  rel = "stylesheet" type = "text/css" >
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+	<div class="wrapper">
+		<section class="form login">
+			<header>Welcome <?php echo $_SESSION['username']; ?></header>
+			<form method="post">
+				<div class="name-details">
+					<div class="field input">
+						<label>Email Address</label>
+						<input type="text" placeholder="Enter your email" required>
+					</div>
+					<div class="field input">
+						<label>Password</label>
+						<input type="password" placeholder="Enter your password" required>
+					</div>
+					<div class="field button">
+						<input type="submit" value="Login" >
+					</div>
+				</div>
+			</form>
+		</section>
+		
+	</div>
+
+	<div>Click here to<a href="../Php/logout.php"> Logout</a></div>
+</body>
+</html>
+
